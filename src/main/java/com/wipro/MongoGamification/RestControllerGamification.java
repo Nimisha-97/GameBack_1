2f0ef422efe4ca35d573307ef2a46116577b19d0
Nimisha Mohapatra <51197219+Nimisha-97@users.noreply.github.com>
2019-08-27 15:59:07 +0530

    Update RestControllerGamification.java

--- src/main/java/com/wipro/MongoGamification/RestControllerGamification.java
+++ src/main/java/com/wipro/MongoGamification/RestControllerGamification.java
@@ -6,6 +6,9 @@ import javax.validation.Valid;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,30 +16,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(value="http://ec2-52-66-189-143.ap-south-1.compute.amazonaws.com:3000")
+
+
+//@CrossOrigin(value="http://ec2-52-66-189-143.ap-south-1.compute.amazonaws.com:3000")
 @RestController
 @RequestMapping("/dashboard")
 public class RestControllerGamification {
 	@Autowired
 	private RepositoryGamification repository;
 	
+	@Autowired
+	private MongoTemplate mongoTemplate;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/teamDash", method = RequestMethod.GET)
 	public List<BeanGamification> getAllTeam() {
 		System.out.println("Nimisha");
-	  return repository.findAll();
+		Query query = new Query();
+		query.addCriteria(Criteria.where("team").is(true));
+	  return mongoTemplate.find(query,BeanGamification.class);
 	}
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public BeanGamification getPetById(@PathVariable("id") ObjectId id) {
-	  return repository.findBy_id(id);
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT) 	
-	public void modifyGameById(@PathVariable("id") ObjectId id, @Valid 
-	@RequestBody BeanGamification game) {
-		game.set_id(id);
-	  repository.save(game);
-	}
+	@RequestMapping(value = "/individualDash", method = RequestMethod.GET)
+	public List<BeanGamification> getAllIndividual() {
+		System.out.println("Nimisha");
+		Query query = new Query();
+		query.addCriteria(Criteria.where("team").is(false));
+	  return mongoTemplate.find(query,BeanGamification.class);
+	}	
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public BeanGamification createGame(@Valid @RequestBody BeanGamification game) {
@@ -48,9 +53,4 @@ public class RestControllerGamification {
 	  return game;
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	  public void deleteGame(@PathVariable ObjectId id) {
-	    repository.delete(repository.findBy_id(id));
-	  }
-
 }
